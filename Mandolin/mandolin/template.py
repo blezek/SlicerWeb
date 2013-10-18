c6db1d1eab0726487c8881376b9ec1062c31cbc6
Daniel Blezek <daniel.blezek@gmail.com>
2013-10-18 14:01:20 -0500

    Do not run .js, .css files through Mako
    Speed boost by sending them directly.

--- Mandolin/mandolin/template.py
+++ Mandolin/mandolin/template.py
@@ -1,20 +1,31 @@
 from app import app
 import slicer
 import json
-from bottle import request, response, HTTP_CODES
+from bottle import request, response, HTTP_CODES, static_file
 import traceback
 from bottle import mako_view as view, mako_template as template
 from bottle import mako_template
 from bottle import MakoTemplate
 import mimetypes
+import os
+
+
+@app.route('/static/<name:path>')
+def simple_static ( name ):
+	return static_file(name, root=app.config['docroot'])
 
 # Run everything through the template engine
 @app.route('/<name:path>')
 def server_static(name):
 	if 'logMessage' in app.config:
-		app.config['logMessage']('handling static file %s' % name)
+		app.config['logMessage']('handling template %s' % name)
 	else:
-		print ('handling static file %s' % name)
+		print ('handling template %s' % name)
+
+	# Don't run .js files through the engine
+	ext = os.path.splitext(name)[-1].lower()
+	if ext in ['.js', '.css']:
+		return static_file(name, root=app.config['docroot'])
 
 	print "Rendering " + name
 	t = MakoTemplate ( name=name, lookup=[app.config['docroot']])
@@ -27,5 +38,4 @@ def server_static(name):
 	return t.render()
 
 
-	return static_file(name, root=app.config['docroot'])
 
