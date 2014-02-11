a6165f0d9b97214a44e815b58b04f3aed1fbaef6
Daniel Blezek <daniel.blezek@gmail.com>
2014-02-11 09:05:18 -0600

    Configuration changes

--- Mandolin/grater/gulpfile.js
+++ Mandolin/grater/gulpfile.js
@@ -1,6 +1,6 @@
 /*
   install:
-  npm install --save-dev gulp gulp-concat gulp-notify gulp-cache gulp-livereload tiny-lr gulp-util express
+  npm install --save-dev gulp gulp-concat gulp-notify gulp-cache gulp-livereload tiny-lr gulp-util express gulp-browserify
   */
 
 
@@ -10,10 +10,11 @@ var gulp = require('gulp'),
     cache = require('gulp-cache'),
     refresh = require('gulp-livereload'),
     livereload = require('gulp-livereload'),
+    uglify = require('gulp-uglify'),
     lr = require('tiny-lr'),
     server = lr();
 
-var js = ['lib/js/**/*.js', 'js/**/*.js']
+var js = ['js/**/*.js']
 var assets = ['assets/**']
 var css = ['css/**/*.css']
 
@@ -43,18 +44,32 @@ gulp.task('lr-server', function() {
   });
 });
 
-gulp.task('default', ['lr-server', 'foundation', 'build'], function() {
+gulp.task('default', ['lr-server', 'vendor', 'build'], function() {
   gulp.watch(all, ['build']);
 })
 
 
-// Build Foundation
-gulp.task('foundation', function() {
+gulp.task('vendor', function() {
+  
+  // Build Foundation
   var foundation = ['bower_components/foundation/js/vendor/jquery.js', 'bower_components/foundation/js/vendor/**/*.js', 'bower_components/foundation/js/foundation.js']
   gulp.src(foundation).pipe(concat('foundation.js')).pipe(gulp.dest('../docroot/js'));
 
   var css = ['bower_components/foundation/css/foundation.css', 'bower_components/foundation/css/normalize.css'];
   gulp.src(css)
   .pipe(gulp.dest('../docroot/css'))
+
+  var js = [
+  'bower_components/underscore/underscore.js',
+  'bower_components/angular/angular.js',
+  'bower_components/backbone/backbone.js',
+  'lib/js/dat.gui.js'
+  ]
+  gulp.src(js).pipe(concat('vendor.js'))
+  .pipe(uglify({outputSourceMap:true}))
+  .pipe(gulp.dest('../docroot/js'))
+
+  gulp.src("lib/js/X/**/*.js").pipe(gulp.dest('../docroot/js/X'))
+
 })
 
