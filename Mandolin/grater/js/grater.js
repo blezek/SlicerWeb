b23872bcac2ae60030338b7c7e535d7232ebed31
Daniel Blezek <daniel.blezek@gmail.com>
2014-02-22 13:51:21 -0600

    Working GUI using Angular

--- Mandolin/grater/js/grater.js
+++ Mandolin/grater/js/grater.js
@@ -33,6 +33,10 @@ require(["model", 'angular', 'angularAMD'], function(model, angular, angularAMD)
 */
 
 
+  render = new X.renderer3D();
+  render.container = "render"
+  render.init();
+
   meshCollection = new model.MeshCollection();
   // setInterval ( function() { meshCollection.fetch({remove: true}) }, 2000 );
 
@@ -40,83 +44,40 @@ require(["model", 'angular', 'angularAMD'], function(model, angular, angularAMD)
   console.log ( "Creating graterApp")
   // create the angular application
   var graterApp = angular.module ( 'graterApp', [] );
+
+  // This is a custom directive to create our GUI as needed
+  graterApp.directive("meshControls", function() {
+    return function(scope, element, attrs) {
+      console.log ( "processing", scope, element, attrs);
+      var m = new X.mesh()
+      m.file = "mrml/data/" + scope.mesh.id + ".stl"
+      render.add ( m )
+
+      var gui, meshGUI;
+      gui = new dat.GUI({ autoPlace: false });
+      meshGUI = gui.addFolder ( scope.mesh.attributes.Name )
+      meshGUI.add( m, 'visible').listen()
+      meshGUI.add( m, 'opacity', 0, 1.0 )
+      // meshGUI.open()
+      element.append( gui.domElement )
+    }
+  })
+
   graterApp.controller ( 'MeshListController', function($scope, $timeout ) {
     meshCollection.fetch({remove: true})
     $scope.meshCollection = meshCollection;
+    $scope.render = render;
     (function tock() {
       meshCollection.fetch({remove:true});
       console.log("tock!")
-      $scope.meshCollection = meshCollection;
+      // $scope.meshCollection = meshCollection;
       $timeout(tock,2000);
     })();
   })
   angularAMD.bootstrap(graterApp)
 
-  render = new X.renderer3D();
-  render.container = "render"
-  render.init();
-
-  // create a cube
-  var cube = new X.cube();
   
-  // setting the edge length can also be skipped since 20 is the default
-  cube.lengthX = cube.lengthY = cube.lengthZ = 20;
-  cube.lengthX = 40;
-  
-  // can also be skipped since [0,0,0] is the default center
-  // cube.center = [0, 0, 0];
-  
-  // [1,1,1] (== white) is also the default so this can be skipped aswell
-  cube.color = [1, 1, 1];
-  
-  render.add(cube); // add the cube to the renderer
-
-
-  // Add the skull
-  var skull = new X.mesh()
-  skull.file = "mrml/data/vtkMRMLModelNode13.stl"
-  skull.opacity = 0.7;
-  render.add(skull)
-
-  var wm = new X.mesh()
-  wm.file = "mrml/data/vtkMRMLModelNode4.stl"
-  render.add(wm)
-
-  render.onShowtime = function() {
-    // Add a GUI
-    var gui, meshGUI;
-    gui = new dat.GUI({ autoPlace: false });
-    var cubeGUI = gui.addFolder ( "Cube")
-    cubeGUI.add(cube, 'visible')
-    var lengthGUI = cubeGUI.add( cube, 'lengthX', 1, 100 ).listen()
-    cubeGUI.open()
-    // $("#drop2").append(gui.domElement)
-    var container = document.getElementById('drop_cube')
-    container.appendChild(gui.domElement)
-
-    gui = new dat.GUI({autoPlace: false});
-    meshGUI = gui.addFolder ( "Mesh" )
-    meshGUI.add( skull, 'visible').listen()
-    meshGUI.add( skull, 'opacity', 0, 1.0 )
-    meshGUI.open()
-    document.getElementById('drop_sb').appendChild ( gui.domElement )
-
-    gui = new dat.GUI({autoPlace: false});
-    meshGUI = gui.addFolder ( "Mesh" )
-    meshGUI.add( wm, 'visible').listen()
-    meshGUI.add( wm, 'opacity', 0, 1.0 )
-    meshGUI.open()
-    document.getElementById('drop_wm').appendChild ( gui.domElement )
-
-    gui = new dat.GUI({autoPlace: false});
-    meshGUI = gui.addFolder("mesh")
-    meshGUI.add(wm, 'visible').listen()
-    meshGUI.open()
-    document.getElementById("test_div").appendChild(gui.domElement)
-
-  }
 
   render.render();
 
-cube.lengthX = 100;
 })
